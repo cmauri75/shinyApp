9294b8b27685d2744f61cd9dfc4fe729b97bf551
Cesare Mauri <cmauri@gmail.com>
2016-02-29 11:35:24 +0100

    links

--- ui.R
+++ ui.R
@@ -18,7 +18,9 @@ shinyUI(
                 div("The basic requirement is that you run 3 km or 2 miles at top of your possibilites, it's important you run all the distance at costant pace, if you must decelerate in last meters or if you don't feel to tired at the end, you should retry the test running slower or faster, respectevely"),
                 div("The route should be as plain as possible (7.5 laps in a ring track is the best)"),
                 div("Once you have the time insert it in the 2 boxes (minutes and seconds) and choosen if you like to use metric or imperial system, the system will calculate in real time the excepeted times."),
-                div("The system will also tell your athlete class (1 is the best for elite runner, 10 is the worst)")
+                div("The system will also tell your athlete class (1 is the best for elite runner, 10 is the worst)"),
+                div("App link:"),a(href="https://cmauri.shinyapps.io/cpShiny/","shinyapps"),
+                div("Github repo: "),a(href="https://github.com/cmauri75/shinyApp","GitHub")
             ),
             mainPanel(
                 h3(textOutput("unit")),
